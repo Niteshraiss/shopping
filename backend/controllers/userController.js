25e630172b74b058d594876febdada7c786b218b
Niteshraiss <niteshrai182@gmail.com>
2021-04-07 17:16:22 +0530

    made changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,71 +1,77 @@
-import asyncHandler from 'express-async-handler';
-import generateToken from '../utils/generateTokens.js'
-import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
 
-//Authentication  or Login
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    console.log('password to match is --', password);
+    const { email, password } = req.body
+
     const user = await User.findOne({ email })
+
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+            token: generateToken(user._id),
         })
-    }
-    else {
+    } else {
         res.status(401)
-        throw new Error('Invalid email or password');
+        throw new Error('Invalid email or password')
     }
-
 })
 
-//registration of an user
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    //getting name ,email,password from body
-    const { name, email, password } = req.body;
+    const { name, email, password } = req.body
 
     const userExists = await User.findOne({ email })
+
     if (userExists) {
-        res.status(400);
-        throw new Error("user already exist");
+        res.status(400)
+        throw new Error('User already exists')
     }
+
     const user = await User.create({
-        name, email, password,
+        name,
+        email,
+        password,
     })
+
     if (user) {
         res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+            token: generateToken(user._id),
         })
     } else {
-        res.status(400);
-        throw new Error("Invalid user data");
+        res.status(400)
+        throw new Error('Invalid user data')
     }
 })
 
-
-//getting user
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id)
+
     if (user) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            isAdmin: user.isAdmin
+            isAdmin: user.isAdmin,
         })
-
     } else {
-        res.status(401);
-        throw new Error("User not found");
+        res.status(404)
+        throw new Error('User not found')
     }
-
 })
-export { getUserProfile, authUser, registerUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,46 +1,45 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-}, {
-    timestamps: true
-})
+    {
+        timestamps: true,
+    }
+)
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+    return await bcrypt.compare(enteredPassword, this.password)
 }
 
-//encrypting password before saving the data
-//this is a middleware
 userSchema.pre('save', async function (next) {
-
-    //checking if only name,email has been modified and if true no need to hash password and skip that step 
     if (!this.isModified('password')) {
-        next();
+        next()
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
+const User = mongoose.model('User', userSchema)
 
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
+export default User
